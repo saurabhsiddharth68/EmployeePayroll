4b3b84059f69cc2dc0f31d2df6cd46fa6ed04ea7
Saurabh Siddharth <saurabhsiddharth68>
2021-11-07 22:20:45 +0530

    Added: Read and Write Employee Payroll

---
+++ src/main/java/com/blz/emppayroll/EmployeePayrollServices.java
@@ -0,0 +1,46 @@
+package com.blz.emppayroll;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class EmployeePayrollServices {
+    public List<EmployeePayrollData> employeePayrollList;
+
+    public EmployeePayrollServices() {
+        employeePayrollList = new ArrayList<>();
+    }
+
+    public static void main(String[] args) {
+        EmployeePayrollServices employeePayrollService = new EmployeePayrollServices();
+        Scanner consoleInputReader = new Scanner(System.in);
+        employeePayrollService.printWelcomeMessage();
+        employeePayrollService.readEmployeePayrollData(consoleInputReader);
+        employeePayrollService.writeEmployeePayrollData();
+    }
+
+    /* This method is implementing Welcome Message */
+    public void printWelcomeMessage() {
+        System.out.println("Welcome To The Employee Payroll Service System");
+    }
+
+    /**
+     * Purpose : To read the information of Employees from the console
+     * @param consoleInputReader takes the information of employees
+     */
+    private void readEmployeePayrollData(Scanner consoleInputReader) {
+        System.out.println("Enter Employee ID : ");
+        int id = consoleInputReader.nextInt();
+        consoleInputReader.nextLine();
+        System.out.println("Enter Employee Name : ");
+        String name = consoleInputReader.nextLine();
+        System.out.println("Enter Employee Salary : ");
+        double salary = consoleInputReader.nextDouble();
+        employeePayrollList.add(new EmployeePayrollData(id, name, salary));
+    }
+
+    // This method is implementing to write the Employee Payroll to the console */
+    private void writeEmployeePayrollData() {
+        System.out.println("\nWriting Employee Payroll Roaster to console\n" + employeePayrollList);
+    }
+}
